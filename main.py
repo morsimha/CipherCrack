9bdc2ffdb992ed5a408ef44b7f8f11583b4dc9fa
morsimha <mor.simha@intel.com>
2022-12-19 15:36:22 +0200

    ready for some guesses

--- main.py
+++ main.py
@@ -12,15 +12,14 @@ def XOR_2_ASCII(a, b,index =0):
         return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a, b[:len(a)])])
 
 
-def crib_2_str(c_1, c_2):
+def crib_2_str(c_1, c_2, exp):
     found = False
     c_1_ascii = convert_to_ascii(c_1)
     c_2_ascii = convert_to_ascii(c_2)
     c1_x_c2_new = XOR_2_ASCII(c_1_ascii, c_2_ascii)
 
-    #words: it was, the , enter,  in t, like, if pl, he i
     for letter, value in list(enumerate(c1_x_c2_new)):
-        final = XOR_2_ASCII("i am ", c1_x_c2_new, letter)
+        final = XOR_2_ASCII(exp, c1_x_c2_new, letter)
 
         if all((123 > ord(c) >= 32) for c in final):
             print("#####################found: " + final)
@@ -34,7 +33,7 @@ def crib_2_str(c_1, c_2):
 if __name__ == '__main__':
     messages_names = ["c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10"]
     messages_content = []
-    with open ("encrypted_messages.txt", "r") as file:
+    with open("encrypted_messages.txt", "r") as file:
         for line in file:
             if not line.startswith("Message"):
                 messages_content.append(line.strip())
@@ -42,14 +41,16 @@ if __name__ == '__main__':
     messages = {}
     for index, value in enumerate(messages_names):
         messages[value] = messages_content[index]
-    print(messages)
-
-    for name, message in messages.items():
-        for name2, message2 in messages.items():
-            if name < name2: # no repetitions
-                found = crib_2_str(message, message2)
-                if found:
-                    print(f"for message {name} and {name2}\n")
+    #  print(messages)
+    expression = "replace me"
+    while expression != "":
+        expression = input("Enter your expression: ")
+        for name, message in messages.items():
+            for name2, message2 in messages.items():
+                if name < name2:  # no repetitions
+                    found = crib_2_str(message, message2, expression)
+                    if found:
+                        print(f"for message {name} and {name2}\n")
 
 #     m1 = "hi how are you? dog.\t2"
 #     m2 = "this is great secret!"
